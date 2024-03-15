059431694a002058a774ab3b59aa3bd4274d0645
Seth Greene <sethandrewg@gmail.com>
2024-03-14 20:18:35 -0400

    Removed unused import

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route, Switch, useParams } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NewReservation from "../newReservation/NewReservation";
 import NotFound from "./NotFound";
